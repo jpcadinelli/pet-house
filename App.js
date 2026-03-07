5526509d2c1113a4676f40af614ddbf210acf937
jpcadinelli <jpcadinelli@gmail.com>
2026-03-07 18:20:22 -0300

    feat: cópia aula do professor

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as LocalAuthentication from 'expo-local-authentication';
+import { useEffect, useState } from 'react';
+
+export function TelaSegura() {
+  const [access, setAccess] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const authentication = await LocalAuthentication.authenticateAsync();
+      if (authentication.success)
+        setAccess(true)
+      else
+        setAccess(false)
+    })();
+  }, []);
+
+  return (
+    <View>
+      {access && (
+        <Text>Usuário logado co sucesso!</Text>
+      )}
+    </View>
+  )
+
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [biometria, setBiometria] = useState(false);
+  const [render, setRender] = useState(false);
+
+  const changeRender = () => setRender(true)
+
+  useEffect(() => {
+    (async () => {
+      const compativel = await LocalAuthentication.hasHardwareAsync();
+      setBiometria(compativel);
+    })();
+  }, []);
+
+  if (render) {
+    return (
+      <TelaSegura />
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text>
+          {biometria
+            ? 'Faça o login com biometria'
+            : 'Dispositivo n"ao cmpativel com biometrias'
+          }
+        </Text>
+        <TouchableOpacity onPress={changeRender}><Text>Logar</Text></TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
