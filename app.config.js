f5fbc6a76367d7d164869259e6d4740f14078103
Pedro Pavanello <pavanellopedro109@gmail.com>
2026-03-21 22:53:28 -0300

    chore: configura chaves de API do Google Maps para Android e iOS

--- app.config.js
+++ app.config.js
@@ -2,15 +2,24 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "pet-house",
-    slug: "pet-house",
-    version: "1.0.0",
+    name: 'pet-house',
+    slug: 'pet-house',
+    version: '1.0.0',
+    orientation: 'portrait',
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.pethouse.app',
+      config: {
+        googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_IOS,
+      },
+    },
     android: {
+      package: 'com.pethouse.app',
       config: {
         googleMaps: {
-          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
-        }
-      }
-    }
-  }
+          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY_ANDROID,
+        },
+      },
+    },
+  },
 };
